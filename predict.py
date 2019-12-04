acf7af2ba8ce5945e4d2366116a974673bbbc64b
Rickymtl <53066199+Rickymtl@users.noreply.github.com>
2019-12-03 19:34:05 -0500

    Update predict.py
    cleanup

--- predict.py
+++ predict.py
@@ -16,9 +16,9 @@ from torchvision import datasets, transforms
 
 # model = CNN3()
 # summary(model, (1,28,28))
+# I put this file into a wrapper for my partner to use.
 def predict_plate(DIR):
-
-    DATADIR = 'English/pred/img1/'
+    
     CATEGORIES = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
                   'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     IMSIZE = 28
@@ -27,16 +27,12 @@ def predict_plate(DIR):
                                     transforms.Grayscale(),
                                     transforms.ToTensor(),
                                     transforms.Normalize((0.5,), (0.5,))])
-    # load the data prediction data
-
-
+    # load the dataset for prediction from DIR
     pred_data = datasets.ImageFolder(root=DIR, transform=transform)
     pred_data_loader = data.DataLoader(pred_data, batch_size=64, shuffle=False)
 
-
+    # show the head of the dataset to make sure the lata is loaded correctly
     image, label = next(iter(pred_data_loader))
-    print(label)
-    print(CATEGORIES[label[0]])
     img = image[0, :]
     img = img.numpy().transpose((1, 2, 0))
     im2 = img[:, :, 0]
@@ -44,8 +40,7 @@ def predict_plate(DIR):
     plt.imshow(im2, 'gray')
     plt.show()
 
-
-
+    # initialize models and load in the weights.
     categorizer = CNN()
     model = CNN3()
     criterion = nn.NLLLoss()
@@ -54,7 +49,7 @@ def predict_plate(DIR):
     categorizer.load_state_dict(torch.load('train1.pt'))
     model.load_state_dict(torch.load('train2.pt'))
 
-
+    
     save_file = 'plates.txt'
     save_file = open(save_file,'w')
 
@@ -63,18 +58,22 @@ def predict_plate(DIR):
     plate = ''
     print(labels)
     for img in images:
+        # check if an image contains a character.
         im = img.unsqueeze(0)
         ps = torch.exp(categorizer(im))
         top_p, top_class = ps.topk(1, dim=1)
-#         helper.view_classify(im, ps, version='CAT')
+        #         helper.view_classify(im, ps, version='CAT')
 
         if top_class.item()==0:
             continue
+        # if code reaches here, there is a character in image.
         im = img.unsqueeze(0)
         ps = torch.exp(model(im))
         top_p, top_class = ps.topk(1, dim=1)
         print(CATEGORIES[top_class.item()])
         print(top_p.item())
+        # some improvements:
+        # I'll detail this in report.
         if top_p.item()<0.5:
             continue
         if CATEGORIES[top_class.item()]=='O':
@@ -88,7 +87,7 @@ def predict_plate(DIR):
         else:
             plate = plate + CATEGORIES[top_class.item()]
         # Plot the image and probabilities
-#         helper.view_classify(im, ps, version='CHAR')
+        # helper.view_classify(im, ps, version='CHAR')
     save_file.write(plate+'\n')
     save_file.close()
     print(plate)
